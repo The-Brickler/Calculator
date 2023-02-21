c1665b2897a8d683f96e8ca1b758861f1c09d8c7
Cami <cbrun.mine@gmail.com>
2023-02-21 13:55:21 -0700

    Starting working out calculations

--- src/calc/controller/Calculator.java
+++ src/calc/controller/Calculator.java
@@ -1,14 +1,35 @@
 package calc.controller;
 
+import java.util.ArrayList;
+
 public class Calculator
 {
-	String expression;
+	private String expression;
+	
+	private final String [] operators = {"+", "-", "*", "/", "%", "^", "r", "x"};
+	
 	
 	public Calculator()
 	{
 		expression = "";
 	}
 	
+	public double calculate()
+	{
+		double answer = 0.0;
+		String finalString;
+		
+		finalString = processParens(expression);
+		
+		return answer;
+	}
+	
+	private double calculate(String input)
+	{
+		double answer = 0.0;
+		
+		return answer;
+	}
 	
 	public String getExpression()
 	{
@@ -28,5 +49,45 @@ public class Calculator
 		}
 		
 		expression = trimmedString;
+	}
+	
+	
+	private String processParens(String input)
+	{
+		String output = "";
+		
+		int firstParen = input.indexOf("(");
+		int lastParen = input.lastIndexOf(")");
+		
+		if (firstParen > -1 && lastParen > -1)
+		{
+			output = input.substring(0, firstParen);
+			output += "" + calculate(input.substring(firstParen + 1, lastParen));
+			if(lastParen < input.length() - 1)
+			{
+				output += input.substring(lastParen + 1);
+			}
+		}
+		return output;
+	}
+	
+	private String processExponents(String input)
+	{
+		String output = "";
+		
+		while (input.indexOf("^") > -1)
+		{
+			int caret = input.indexOf("^");
+			
+			ArrayList<Integer> intList = new ArrayList<>();
+			
+			for (String operator : operators)
+			{
+				intList.add(input.indexOf(operator));
+			}
+			
+		}
+		
+		return output;
 	}
 }
